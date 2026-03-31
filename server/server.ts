3bf5ea33f9cd759fa2a918e9dc31ebe5ec545a8a
Isaac Laster <isaac.laster812@gmail.com>
2026-03-30 19:57:26 -0700

    made sure to put in 0.0.0.0

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,6 @@
 import { app } from "./app.ts";
 import { env } from "./config/env.ts";
 
-app.listen(env.port, () => {
+app.listen(env.port, "0.0.0.0", () => {
   console.log(`Server running on http://localhost:${env.port}`);
 });
