0ce6b9e5bcb02948269f05661e26b1f074189e9a
Isaac Laster <isaac.laster812@gmail.com>
2026-04-08 14:33:14 -0700

    chunk debugging

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          zxing: ["@zxing/library"],
+          react: ["react", "react-dom"],
+        },
+      },
+    },
+  },
+});
